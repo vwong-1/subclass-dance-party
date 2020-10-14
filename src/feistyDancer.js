7a0e0e9047ccf0eb19f7ca4f890a8e2e2d83c62a
Victor Wong <victorw1992@gmail.com>
2020-10-13 17:38:53 -0700

    Implented rotating ticking cat

--- src/feistyDancer.js
+++ src/feistyDancer.js
@@ -3,13 +3,21 @@ var makeFeistyDancer = function(top, left, timeBetweenSteps) {
   this.$node.addClass('feistyDancer');
   this.$image = $('<img class="cat" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn2.scratch.mit.edu%2Fget_image%2Fgallery%2F838908_170x100.png&f=1&nofb=1">');
   this.$node.append(this.$image);
+  this.angle = 0;
 };
 
 makeFeistyDancer.prototype = Object.create(makeDancer.prototype);
 makeFeistyDancer.prototype.constructor = makeFeistyDancer;
 
 makeFeistyDancer.prototype.helper = function() {
-  this.$node.toggle();
+  this.angle += 45;
+  this.$node.css({
+    transform: `rotate(${this.angle}deg)`
+  });
+  // this.$node.rotate({
+  //   angle: 0,
+  //   animateTo: 180
+  // });
 };
 
 // makeFeistyDancer.prototype.step = function() {
